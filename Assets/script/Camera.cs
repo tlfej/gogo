dc608159994b6b8f27e2be954fb5fca149a1eec8
tlfej <tlfej21@gmail.com>
2017-07-14 16:44:58 +0900

    dat01
    chrater

---
+++ Assets/script/Camera.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform player;
+    public float dist = 5f;
+    public float height = 2f;
+    public float rotspeed = 5.0f;
+
+    float radi = 0;
+    Vector3 movedir;
+    Vector3 mousepo;
+    private Transform tr;
+    Vector3 mouseim;
+
+    // Use this for initialization
+    void Start () {
+        tr = GetComponent<Transform>();
+      
+	}
+   
+    // Update is called once per frame
+    void LateUpdate () {
+        tr.position = Vector3.Lerp(tr.position, player.position - (player.forward * dist) + Vector3.up * height, Time.deltaTime * 20.0f);
+
+        /*
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseim = Input.mousePosition;
+          
+        }
+        if (Input.GetMouseButton(0))
+        {
+            mousepo = Input.mousePosition;
+            if (mouseim.x < mousepo.x)
+            {
+                if (radi > 360) radi = 0;
+                radi += 10f;
+
+                mouseim = mousepo;
+
+            }
+            else if (mouseim.x > mousepo.x)
+            {
+                if (radi < -1*360) radi = 0;
+                radi += -1 * 10f;               
+                 mouseim = mousepo;
+            }
+            movedir = (player.transform.forward * Mathf.Sign(radi) + player.transform.right * Mathf.Cos(radi)).normalized * dist;
+        }
+
+        tr.position = Vector3.Lerp(tr.position, (player.position +movedir+ Vector3.up * height), Time.deltaTime * 20.0f);
+
+
+
+        */
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    mouseim = Input.mousePosition;
+        //    Debug.Log(Input.mousePosition);
+        //}
+        //if (Input.GetMouseButton(0))
+        //{
+        //    mousepo = Input.mousePosition;
+        //    if (mouseim.x < mousepo.x)
+        //    {
+        //        radi = 10f;
+        //        tr.transform.RotateAround(player.transform.position, Vector3.up, radi);
+        //        //Debug.Log(radi);
+        //        // tr.Rotate(player.transform.up * radi*Time.deltaTime);
+        //        // tr.rotation = Quaternion.Euler(tr.up * radi * Time.deltaTime);
+        //        // mouseim = mousepo;
+
+        //    }
+        //    else if (mouseim.x > mousepo.x)
+        //    {
+        //        radi = -1 * 10f;
+        //        tr.transform.RotateAround(player.transform.position, Vector3.up, radi);
+        //        //Debug.Log(radi);
+        //        // tr.Rotate(Vector3.up * radi * Time.deltaTime );
+        //        //tr.rotation = Quaternion.Euler(tr.up * radi * Time.deltaTime);
+        //        // mouseim = mousepo;
+        //    }
+
+        //     }
+
+
+        //    //// movedir = (.forward * Mathf.Sign(radi) + Vector3.right * Mathf.Cos(radi)).normalized * dist;
+        //    // if (Input.GetMouseButtonDown(0))
+        //    // {
+        //    //     mouseim = Input.mousePosition;
+        //    //     Debug.Log(Input.mousePosition);
+        //    // }
+        //    // if (Input.GetMouseButton(0))
+        //    // {
+        //    //     mousepo = Input.mousePosition;
+        //    //     if(mouseim.x < mousepo.x)
+        //    //     {
+        //    //         radi = 20f;
+        //    //         //Debug.Log(radi);
+        //    //         // tr.Rotate(player.transform.up * radi*Time.deltaTime);
+        //    //         tr.rotation = Quaternion.Euler(tr.up * radi * Time.deltaTime );
+        //    //         mouseim = mousepo;
+
+        //    //     }
+        //    //     else if(mouseim.x >mousepo.x)
+        //    //     {
+        //    //         radi = -1*20f;
+        //    //         //Debug.Log(radi);
+        //    //         // tr.Rotate(Vector3.up * radi * Time.deltaTime );
+        //    //         tr.rotation = Quaternion.Euler(tr.up * radi * Time.deltaTime );
+        //    //         mouseim = mousepo;
+        //    //     }
+
+        //    // }
+
+        //    // /*
+        //    // if (Input.GetMouseButton(0))
+        //    // {
+        //    //     Debug.Log("눌려있당");
+        //    // }*/
+
+        //     tr.position = Vector3.Lerp(tr.position,(player.position - player.transform.forward*dist +Vector3.up *height),Time.deltaTime*20.0f) ;
+
+    }
+}
---
+++ Assets/script/ChaterCtrl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//캐릭터 상태 enum변수
+[System.Serializable]
+public class Anim
+{
+    public AnimationClip wait;
+    public AnimationClip walk;
+    public AnimationClip attack;
+    public AnimationClip damage;
+    public AnimationClip dead;
+}
+
+
+public class ChaterCtrl : MonoBehaviour {
+    private Transform tr;
+    private Rigidbody rbody;
+
+    public float movespeed = 2.0f;
+    public float rotspeed= 100f;
+
+    private GameObject ca;
+    public Anim anim;
+    public Animation _animation;
+
+    
+
+
+    Vector3 movedir;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+        rbody = GetComponent<Rigidbody>();
+        _animation = GetComponent<Animation>();
+        _animation.clip = anim.wait;
+        _animation.Play();
+        ca = GameObject.Find("Main Camera");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movedir = (tr.forward *Input.GetAxis("Vertical") ) + (tr.right * Input.GetAxis("Horizontal")).normalized;
+        //movedir = (Vector3.forward * Input.GetAxis("Vertical")) + (Vector3.right * Input.GetAxis("Horizontal") );
+        tr.Rotate(Vector3.up * Time.deltaTime * rotspeed * Input.GetAxis("Horizontal"));
+        
+
+        rbody.velocity = movedir * movespeed;
+		if( Input.GetAxis("Horizontal") !=0 || Input.GetAxis("Vertical")!= 0)
+        {
+           
+            ca.transform.LookAt(tr.position);
+            _animation.CrossFade(anim.walk.name, 0.3f);
+
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            _animation.CrossFade(anim.attack.name, 0.3f);
+        }
+        else
+        {
+            _animation.CrossFade(anim.wait.name, 0.3f);
+        }
+       
+	}
+}
